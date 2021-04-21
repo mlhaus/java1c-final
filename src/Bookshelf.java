50e61d83ded09bce7575e0c54335bd26a3b778cb
Marc Hauschildt <marc.hauschildt@gmail.com>
2021-04-20 20:38:29 -0500

    Created the Bookshelf class

---
+++ src/Bookshelf.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bookshelf {
+    private Book[] bookshelf;
+    // private List<Book> bookshelf;
+
+    public Bookshelf() {
+        bookshelf = new Book[10];
+        // bookshelf = new ArrayList<>(10);
+    }
+
+    public Bookshelf(int numBooks) {
+        bookshelf = new Book[numBooks];
+    }
+
+    public static void addBook(Book book) {
+        
+    }
+
+    public static void listBooksLeftToRight() {
+
+    }
+
+    public static void listBooksAlphabetically() {
+
+    }
+
+    public static void listBooksReadUnread() {
+
+    }
+
+    public static void listBooksByType() {
+
+    }
+
+    public static void markBookAsRead(int bookIndex) {
+
+    }
+
+    public static void moveBook(int bookIndex, int newIndex) {
+
+    }
+
+    public static void swapBooks(int book1Index, int book2Index) {
+
+    }
+
+    public static void removeBook(int bookIndex) {
+
+    }
+}
